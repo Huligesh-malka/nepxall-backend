10e1679513e2a3a0fae4d0a61457582f7d0f0589
Huligesh Malka <huligeshmalka@gmail.com>
2026-02-26 22:41:22 +0530

    cashfree production fix

--- cashfree.js
+++ cashfree.js
@@ -1,13 +1,9 @@
-const { Cashfree } = require("cashfree-pg");
+const { Cashfree, CFEnvironment } = require("cashfree-pg");
 
-// ✅ set credentials
-Cashfree.XClientId = process.env.CASHFREE_APP_ID;
-Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
+const cashfree = new Cashfree(
+  CFEnvironment.PRODUCTION,
+  process.env.CASHFREE_APP_ID,
+  process.env.CASHFREE_SECRET_KEY
+);
 
-// ✅ set environment
-Cashfree.XEnvironment =
-  process.env.CASHFREE_ENV === "PRODUCTION"
-    ? Cashfree.Environment.PRODUCTION
-    : Cashfree.Environment.SANDBOX;
-
-module.exports = Cashfree;
+module.exports = cashfree;
