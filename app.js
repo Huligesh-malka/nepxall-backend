23d3efe9c67d0599c039f19a0da9acc4f1ee54e6
Huligesh Malka <huligeshmalka@gmail.com>
2026-02-28 14:48:05 +0530

    added policy pages

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions))
 
 /* ================= ROOT ================= */
 app.get("/", (req, res) => {
