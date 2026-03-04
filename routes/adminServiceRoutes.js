612ade0fb7b998451ebbd116201efcbaf465c8fa
Huligesh Malka <huligeshmalka@gmail.com>
2026-03-04 09:29:02 +0530

    Added services route

--- routes/adminServiceRoutes.js
+++ routes/adminServiceRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
-const controller = require("../controllers/adminServiceController");
+const adminServiceController = require("../controllers/adminServiceController");
 
-router.get("/services", auth, controller.getAllServiceBookings);
-router.get("/vendors", auth, controller.getVerifiedVendors);
-router.post("/assign-vendor", auth, controller.assignVendor);
-router.get("/summary", auth, controller.getServiceSummary);
+router.get("/services", auth, adminServiceController.getAllServiceBookings);
+router.get("/vendors", auth, adminServiceController.getVendors);
+router.post("/assign-vendor", auth, adminServiceController.assignVendor);
 
 module.exports = router;
