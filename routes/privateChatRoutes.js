eaa8475064ef9c2a6eed6486cad8e37b8d64119a
Huligesh Malka <huligeshmalka@gmail.com>
2026-03-10 23:19:11 +0530

    Fix UPI QR compatibility

--- routes/privateChatRoutes.js
+++ routes/privateChatRoutes.js
@@ -2,30 +2,30 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middlewares/auth");
-const chat = require("../controllers/privateChatController");
+const privateChat = require("../controllers/privateChatController");
 
 /* AUTH + USER LOAD */
-router.use(auth, chat.loadMe);
+router.use(auth, privateChat.loadMe);
 
 /* CURRENT USER */
-router.get("/me", chat.getMe);
+router.get("/me", privateChat.getMe);
 
 /* CHAT LIST */
-router.get("/list", chat.getMyChatList);
+router.get("/list", privateChat.getMyChatList);
 
-/* GET USER */
-router.get("/user/:id", chat.getUserById);
+/* GET USER + PG */
+router.get("/user/:id/:pgId", privateChat.getUserById);
 
 /* GET MESSAGES */
-router.get("/messages/:userId", chat.getPrivateMessages);
+router.get("/messages/:userId/:pgId", privateChat.getPrivateMessages);
 
 /* SEND MESSAGE */
-router.post("/send", chat.sendPrivateMessage);
+router.post("/send", privateChat.sendPrivateMessage);
 
 /* UPDATE MESSAGE */
-router.put("/message/:id", chat.updatePrivateMessage);
+router.put("/message/:id", privateChat.updatePrivateMessage);
 
 /* DELETE MESSAGE */
-router.delete("/message/:id", chat.deletePrivateMessage);
+router.delete("/message/:id", privateChat.deletePrivateMessage);
 
 module.exports = router;
