4a4acdee5ac455186d5ba89e16b4baf103139c16
Huligesh Malka <huligeshmalka@gmail.com>
2026-03-12 22:00:03 +0530

    Fix UPI QR compatibility

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 
 /* ================= SAFE ROUTE LOADER ================= */
 
