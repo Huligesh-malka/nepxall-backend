e244d5fc577653e54263665bc995ffa713a3d0a8
Huligesh Malka <huligeshmalka@gmail.com>
2026-04-13 13:13:59 +0530

    Fix UPI QR compatibility

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/authMiddleware");
+const adminAuth = require("../middlewares/adminAuth");
 
 
 const {
