aa037a00da52d88a9d0eec2064d5491a7e124e30
Huligesh Malka <huligeshmalka@gmail.com>
2026-04-28 20:17:50 +0530

    Fix UPI QR compatibility

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,17 +1,26 @@
 const admin = require("firebase-admin");
 
 //////////////////////////////////////////////////////
-// 🔥 SERVICE ACCOUNT
-//////////////////////////////////////////////////////
-const serviceAccount = require("../serviceAccountKey.json");
-
-//////////////////////////////////////////////////////
 // 🔥 INITIALIZE FIREBASE ADMIN
 //////////////////////////////////////////////////////
 if (!admin.apps.length) {
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+
+    credential: admin.credential.cert({
+
+      projectId:
+        process.env.FIREBASE_PROJECT_ID,
+
+      clientEmail:
+        process.env.FIREBASE_CLIENT_EMAIL,
+
+      privateKey:
+        process.env.FIREBASE_PRIVATE_KEY
+          .replace(/\\n/g, '\n')
+
+    })
+
   });
 
 }
